7dbccff40e558072086f1b54f65350a183763a18
RamiroDAgnone <ramiro.dagnone@gmail.com>
2022-08-24 19:13:15 -0300

    login y registro

--- app.js
+++ app.js
@@ -13,5 +13,9 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/register.html"))
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+
 app.listen(3000, () =>
     console.log("Servidor corriendo en el puerto 3000"));
