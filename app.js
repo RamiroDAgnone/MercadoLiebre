5bd026bcf1e46a15ad1de2d8b11abab2d4505874
RamiroDAgnone <ramiro.dagnone@gmail.com>
2022-08-29 20:13:23 -0300

    heroku

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"))
 });
 
-app.listen(3000, () =>
+app.listen(process.env.PORT || 3000, () =>
     console.log("Servidor corriendo en el puerto 3000"));
